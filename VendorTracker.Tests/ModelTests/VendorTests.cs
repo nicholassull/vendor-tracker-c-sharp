11875aeaf907df691f698a524bf4e26841b7bf79
Nick Sullivan <nicholassull@gmail.com>
2022-02-25 15:21:45 -0800

    add Vendor.GetAll() and tests

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -38,6 +38,19 @@ namespace VendorTracker.Tests
 
       Assert.AreEqual(1, result);
     }
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+      {
+        string name01 = "test one";
+        string name02 = "test two";
+        Vendor newVendor1 = new Vendor(name01);
+        Vendor newVendor2 = new Vendor(name01);
+        List<Vendor> vendorList = new List<Vendor> {newVendor1, newVendor2};
 
+        List<Vendor> result = Vendor.GetAll();
+        
+        CollectionAssert.AreEqual(vendorList, result);
+      }
+    
   }
 }
--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -15,6 +15,10 @@ namespace VendorTracker.Models
       _instances.Add(this);
       Id = _instances.Count;
     }
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
     public static void ClearAll()
     {
       _instances.Clear();
