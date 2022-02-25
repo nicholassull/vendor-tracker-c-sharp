351870929e6522022078a22ec0cb3680bfe5e3be
Nick Sullivan <nicholassull@gmail.com>
2022-02-25 15:10:14 -0800

    add Vendor name retreival test

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -14,5 +14,15 @@ namespace VendorTracker.Tests
       Vendor newVendor = new Vendor("test Vendor");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "test Vendor";
+      Vendor newVendor = new Vendor(name);
+
+      string result = newVendor.Name;
+
+      Assert.AreEqual(name, result);
+    }
   }
 }
