4231c777ea790d4b50c8897511248e36e138242d
Nick Sullivan <nicholassull@gmail.com>
2022-02-25 16:32:47 -0800

    add splash page, vendor page, and initial routing

---
+++ VendorTracker/Controllers/VendorsController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorTracker.Models;
+using System.Collections.Generic;
+
+namespace VendorTracker.Controllers
+{
+  public class VendorController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+  }
+}
